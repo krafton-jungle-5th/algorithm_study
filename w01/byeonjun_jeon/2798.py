e3aa5324ae051b2d3c6b033bf2baabab606f1167
jun9898 <nice1998@gmail.com>
2024-02-13 16:55:27 +0900

    #1 byeongjun_2798 > https://www.acmicpc.net/problem/2798

---
+++ w01/byeonjun_jeon/2798.py
@@ -0,0 +1,22 @@
+import math
+from itertools import *
+
+n, m = map(int, input().split(" "))
+
+numArr = list(map(int, input().split(" ")))
+
+# 3중 for문?
+resultArr = list(combinations(numArr, 3))
+
+bench = math.inf
+
+result = 0
+
+# m값 - sum(i)의 절대값이 적으면 저장
+for i in resultArr:
+    total = sum(i)
+    if total <= m and m-total < bench:
+        result = total
+        bench = m-total
+
+print(result)
