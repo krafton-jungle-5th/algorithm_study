21bf2c97fde37d5fda25fcd2e50932e720b0bc9e
park-yina <111878820+park-yina@users.noreply.github.com>
2024-02-13 15:38:48 +0900

    Feat:그룹단어체커
    그룹단어 체커 문제의 코드입니다.
    c++의 경우 코드만 보시려면 FileName.cpp을 열면 통상적으로 아시는 코드 보기가 나옵니다:)

---
+++ w01/park_yina/FileName.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<set>
+#include<string>
+#include<algorithm>
+using namespace std;
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int n;
+int cnt;
+int main() {
+	FAST;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		string str2;
+		str2 = str;
+		auto last = unique(str.begin(), str.end());
+		str.erase(last, str.end());
+		set<char>s(str2.begin(), str2.end());
+		if (s.size() == str.length()) {
+			cnt++;
+		}
+	}
+	cout << cnt;
+}
