db639b997fa6d6ed11bdb39cb16ec08a8e55bbd1
syk25 <syk2245@gmail.com>
2024-02-14 04:23:15 +0900

    #1 syk25_1978 https://www.acmicpc.net/problem/1978

---
+++ w01/1978.py
@@ -0,0 +1,24 @@
+n = int(input())
+count = 0
+
+data = list(map(int, input().split()))
+
+for i in range(n):
+    if data[i] == 1:
+        continue
+    elif data[i] == 2:
+        count += 1
+    elif data[i] % 2 == 0:
+        continue
+    else:
+        is_prime = True
+        for j in range(3, data[i] // 2 + 1):
+            if data[i] % j == 0:
+                is_prime = False
+                continue
+        if is_prime:
+            count += 1
+
+
+print(count)
+
