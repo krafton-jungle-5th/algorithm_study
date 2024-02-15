0cfb2ca7b91426b76abeb2c3a3283a690e4375ee
park-yina <111878820+park-yina@users.noreply.github.com>
2024-02-15 13:14:05 +0900

    #1 parkyina_10870 https://www.acmicpc.net/problem/10870
    메모제이션 활용 코드 업로드

--- w01/park_yina/피보나치수5.cpp
+++ w01/park_yina/피보나치수5.cpp
@@ -12,3 +12,35 @@ int main() {
 	cin >> input;
 	cout<<solve(input);
 }
+//최적화 이후 코드(메모제이션 기법 활용 코드)
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> memo;
+
+int solve(int n) {
+    if (memo[n] != -1) 
+        return memo[n];
+    
+    if (n == 0 || n == 1)
+        return n;
+    else {
+        memo[n] = solve(n - 1) + solve(n - 2);
+        return memo[n];
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int input = 0;
+    cin >> input;
+    
+    memo.assign(input + 1, -1); // 메모 배열 초기화
+    cout << solve(input);
+    
+    return 0;
+}
